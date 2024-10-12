ba5728b0e4bd54efc105cbd6e5cdb7553419e491
rafaverde <rafaverde@msn.com>
2024-10-12 07:27:58 -0300

    Toaster configured with sonner. Using on signIn function.

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,6 +1,7 @@
 import { Helmet } from "react-helmet-async";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import { toast } from "sonner";
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
@@ -20,8 +21,19 @@ export function SignIn() {
   } = useForm<SignInFormInput>();
 
   async function handleSignIn(data: SignInFormInput) {
-    console.log(data);
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+    try {
+      console.log(data);
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+
+      toast.success("Enviamos um link de autenticação para seu email.", {
+        action: {
+          label: "Reenviar",
+          onClick: () => handleSignIn(data),
+        },
+      });
+    } catch {
+      toast.error("Credenciais inválidas!");
+    }
   }
   return (
     <>
