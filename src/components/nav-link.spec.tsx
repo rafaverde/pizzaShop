674d683e355dd4d82cb3ac5b4856c16d920f3055
rafaverde <rafaverde@msn.com>
2024-11-04 11:37:51 -0300

    Pagination and NavLink tests created.

---
+++ src/components/nav-link.spec.tsx
@@ -0,0 +1,24 @@
+import { render } from "@testing-library/react";
+import { NavLink } from "./nav-ilnk";
+import { MemoryRouter } from "react-router-dom";
+
+describe("Navlink", () => {
+  it("should highlight the current page link on nav menu", () => {
+    const wrapper = render(
+      <>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/orders">Pedidos</NavLink>
+      </>,
+      {
+        wrapper: ({ children }) => {
+          return (
+            <MemoryRouter initialEntries={["/orders"]}>{children}</MemoryRouter>
+          );
+        },
+      },
+    );
+
+    expect(wrapper.getByText("Home").dataset.current).toEqual("false");
+    expect(wrapper.getByText("Pedidos").dataset.current).toEqual("true");
+  });
+});
