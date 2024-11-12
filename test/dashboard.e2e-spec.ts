bfdd30ed669dfe262472354fc14139bf8c151a34
rafaverde <rafaverde@msn.com>
2024-11-12 12:10:36 -0300

    Dashboard metrics e2e tests created.

---
+++ test/dashboard.e2e-spec.ts
@@ -0,0 +1,29 @@
+import { test, expect } from "@playwright/test";
+
+test("display monthly revenue amount metrics", async ({ page }) => {
+  await page.goto("/", { waitUntil: "networkidle" });
+
+  expect(page.getByText("R$ 200,49")).toBeVisible();
+  expect(page.getByText("-1% em relação ao mês passado")).toBeVisible();
+});
+
+test("display monthly orders amount metrics", async ({ page }) => {
+  await page.goto("/", { waitUntil: "networkidle" });
+
+  expect(page.getByText("201", { exact: true })).toBeVisible();
+  expect(page.getByText("+10% em relação ao mês passado")).toBeVisible();
+});
+
+test("display daily orders amount metrics", async ({ page }) => {
+  await page.goto("/", { waitUntil: "networkidle" });
+
+  expect(page.getByText("20", { exact: true })).toBeVisible();
+  expect(page.getByText("-5% em relação a ontem")).toBeVisible();
+});
+
+test("display monthly canceled orders amount metrics", async ({ page }) => {
+  await page.goto("/", { waitUntil: "networkidle" });
+
+  expect(page.getByText("5", { exact: true })).toBeVisible();
+  expect(page.getByText("-10% em relação ao mês passado")).toBeVisible();
+});
